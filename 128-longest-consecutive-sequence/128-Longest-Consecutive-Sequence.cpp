543b45e4f59141ac80a780a5eff32f2002b6fa08
SAGNIK SAHA <126655769+sagnik81599@users.noreply.github.com>
2026-07-13 11:20:10 +0530

    [128] [Time Beats: 95.81%] [Memory Beats: 84.94%] - LeetPush

---
+++ 128-longest-consecutive-sequence/128-Longest-Consecutive-Sequence.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        if(nums.empty()){
+            return false;
+        }
+
+        sort(nums.begin(),nums.end());
+
+        int count =1;
+        int maxcount =1;
+
+        for(int i=1;i<nums.size();i++){
+        if(nums[i]==nums[i-1])
+            continue;
+        
+        if(nums[i]==nums[i-1]+1){
+            count++;
+        }
+        else{
+            count=1;
+        }
+        maxcount= max(maxcount,count);
+
+        }
+        return maxcount;
+    }
+    
+};
