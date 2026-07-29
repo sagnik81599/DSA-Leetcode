d5f47f7a9571693a67ca8499086d875e1420eeab
SAGNIK SAHA <126655769+sagnik81599@users.noreply.github.com>
2026-07-29 23:03:39 +0530

    [27] [Time Beats: 100.00%] [Memory Beats: 13.00%] - LeetPush

---
+++ 27-remove-element/27-Remove-Element.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int n = nums.size();
+        int cnt = 0;
+        for(int i=0;i<n;i++) {
+            if(nums[i] != val) {
+                nums[cnt] = nums[i];
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
