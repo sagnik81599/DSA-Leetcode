d904b6a25d72bb30d65dfbe2b73202b9decb9e49
SAGNIK SAHA <126655769+sagnik81599@users.noreply.github.com>
2026-09-06 01:45:58 +0530

    [461] [Time Beats: 100.00%] [Memory Beats: 4.68%] - LeetPush

---
+++ 461-hamming-distance/461-Hamming-Distance.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int result = x^y;
+        int count = 0;
+
+        while(result>0){
+            if(result % 2 == 1){
+                count++;
+            }
+
+            result = result>>1;
+
+        }
+
+        return count;
+    }
+};
